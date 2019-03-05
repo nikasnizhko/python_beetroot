8f384e3ba9b0e8ebf72a25ed6dc26e5e83dd3a56
nikasnizhko <nika.snizhok@gmail.com>
2019-03-05 22:25:53 +0200

    initial commit

---
+++ lesson7_homework.py
@@ -0,0 +1,34 @@
+#Task.1 Dict comprehension exercise.
+
+
+user_input = input("Enter your text: ")
+text = user_input.split()
+
+new_dict = {word: text.count(word) for word in text}
+print(new_dict)
+
+
+# new_dict = dict()
+# for word in user_input.split():
+#     new_dict[word] = new_dict.get(word, 0) + 1
+# print(new_dict)
+
+
+
+#Task.2 List comprehension exercise I
+
+
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+b = [number for number in a if number%2 != 0]
+
+print(b)
+
+
+#Task.3 List comprehension exercise II
+
+
+list_1 = [(i,i**2) for i in range(1,11)]
+print(list_1)
+
+
+
