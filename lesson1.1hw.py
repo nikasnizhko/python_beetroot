5ffad27828b26c2c7a8480447ccdc3ec4ec713ed
nikasnizhko <nika.snizhok@gmail.com>
2019-03-13 14:41:39 +0200

    initial commit

---
+++ lesson1.1hw.py
@@ -0,0 +1,21 @@
+# Task 1. A person class
+
+
+class Person:
+    def __init__(self, firstname, lastname, age):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.age = age
+
+
+    def talk(self):
+        print(f"Hello, my name is {self.firstname} {self.lastname} and I'm {self.age} years old")
+
+
+person1 = Person("Carl", "Johnsn", "26")
+
+
+print(person1.talk())
+
+
+# ?? unclear why get None as a result
